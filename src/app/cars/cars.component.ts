03d9e5047e59de42d432955bc21825d61ea8abf4
Nikita Munko <nikmoon16@gmail.com>
2025-06-01 23:42:40 +0200

    b2l8

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,14 +1,15 @@
 import {Component} from '@angular/core';
 import {CarComponent} from '../car/car.component';
 import {FormsModule} from '@angular/forms';
-import {NgIf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-cars',
   imports: [
     CarComponent,
     FormsModule,
-    NgIf
+    NgIf,
+    NgForOf
   ],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
@@ -17,12 +18,16 @@ export class CarsComponent {
 
   carName: string = '';
   addCarStatus: boolean = false;
+  cars = ['Ford', 'Audi', 'BMW']
+  items = [{id : 3, name : 'item 1'}, {id : 6, name : 'item 2'}, {id : 9, name : 'item 3'}];
 
   constructor() {
   }
 
   addCar() {
     this.addCarStatus = true;
+    this.cars.push(this.carName);
+    this.carName = '';
   }
 
 }
