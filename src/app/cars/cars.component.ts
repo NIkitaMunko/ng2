430ed07f4ee691eefe368fe1dcdb88508c410be4
Nikita Munko <nikmoon16@gmail.com>
2025-06-01 23:30:03 +0200

    b2l7 - ngIf else

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,30 +1,28 @@
 import {Component} from '@angular/core';
 import {CarComponent} from '../car/car.component';
 import {FormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-cars',
   imports: [
     CarComponent,
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
 })
 export class CarsComponent {
 
-  addCarStatus: string = '';
-  inputText: string = 'Default text';
+  carName: string = '';
+  addCarStatus: boolean = false;
 
   constructor() {
   }
 
   addCar() {
-    this.addCarStatus = 'Машина добавлена'
-  }
-
-  onKeyUp(event: Event) {
-    // console.log(event.)
+    this.addCarStatus = true;
   }
 
 }
