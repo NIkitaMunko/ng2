0947d824744797b74cb89526323e3a045d297a4b
Nikita Munko <nikmoon16@gmail.com>
2025-06-02 00:00:52 +0200

    b2l10 - pipes

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {CarComponent} from '../car/car.component';
 import {FormsModule} from '@angular/forms';
-import {NgClass, NgForOf, NgIf, NgStyle} from '@angular/common';
+import {DatePipe, NgClass, NgForOf, NgIf, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-cars',
@@ -11,7 +11,8 @@ import {NgClass, NgForOf, NgIf, NgStyle} from '@angular/common';
     NgIf,
     NgForOf,
     NgStyle,
-    NgClass
+    NgClass,
+    DatePipe
   ],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
@@ -21,6 +22,12 @@ export class CarsComponent {
   carName: string = '';
   addCarStatus: boolean = false;
   cars = ['Ford', 'Audi', 'BMW', 'Mazda', 'Lada', 'Bently']
+  dates = [
+    new Date(2015, 3, 4),
+    new Date(2011, 4, 4),
+    new Date(2017, 5, 4),
+    new Date(2010, 7, 4),
+  ];
 
   constructor() {
   }
@@ -29,10 +36,6 @@ export class CarsComponent {
     this.addCarStatus = true;
     this.cars.push(this.carName);
     this.carName = '';
-  }
-
-  setBigCarText(car: string) {
-    return car.length > 4;
   }
 
 }
