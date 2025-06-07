e7e059ee222239e660d0ff1db43f5439f7346c75
Nikita Munko <nikmoon16@gmail.com>
2025-06-07 18:56:35 +0200

    b3l3

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {CarComponent} from '../car/car.component';
 import {NgForOf} from '@angular/common';
@@ -13,7 +13,8 @@ import {AddCarComponent} from '../add-car/add-car.component';
     AddCarComponent,
   ],
   templateUrl: './cars.component.html',
-  styleUrl: './cars.component.css'
+  styleUrl: './cars.component.css',
+  // encapsulation: ViewEncapsulation.None
 })
 export class CarsComponent {
 
