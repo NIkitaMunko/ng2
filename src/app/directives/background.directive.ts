3a19935f8eff675a34f5520e2ed14a1f8e9f715f
Nikita Munko <nikmoon16@gmail.com>
2025-06-08 10:44:02 +0200

    b4l4

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,23 +1,26 @@
-import {Directive, ElementRef, Host, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBackground]',
 })
 export class BackgroundDirective implements OnInit {
 
+  @Input('appBackground') hoverColor: string = 'green';
+  @Input() defaultColor: string = 'transparent';
   @HostBinding('style.backgroundColor') background!: string;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {
   }
 
   ngOnInit(): void {
+    this.background = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseEnter(): void {
-    this.background = 'green';
+    this.background = this.hoverColor;
   }
 
   @HostListener('mouseleave') mouseLeave(): void {
-    this.background = 'transparent';
+    this.background = this.defaultColor;
   }
 }
