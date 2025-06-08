70ca2e71d95976bf4046ebd8acb2ea5252a6f673
Nikita Munko <nikmoon16@gmail.com>
2025-06-08 14:23:04 +0200

    b5L1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,66 @@
 import {Component} from '@angular/core';
-import {BackgroundDirective} from './directives/background.directive';
-import {NgForOf, NgSwitch, NgSwitchCase, NgSwitchDefault} from '@angular/common';
+import {DatePipe, JsonPipe, PercentPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [
-    BackgroundDirective,
-    NgForOf,
-    NgSwitch,
-    NgSwitchCase,
-    NgSwitchDefault
+    DatePipe,
+    DatePipe,
+    PercentPipe,
+    JsonPipe
   ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  template: `
+    <div class="col-xs-8 col-xs-offset-2">
+<!--      <h2>{{ name }}</h2>-->
+<!--      <h2>{{ name | lowercase }}</h2>-->
+<!--      <h2>{{ name | uppercase }}</h2>-->
+<!--      <h2>{{ name | slice:0:3 }}</h2>-->
+<!--      <h2>{{ name | slice:3:6 | uppercase }}</h2>-->
+<!--      <hr>-->
+<!--      <h2>{{ pi }}</h2>-->
+<!--      <h2>{{ pi | number }}</h2>-->
+<!--      <h2>{{ pi | number:'1.3-4' }}</h2>-->
+<!--      <h2>{{ pi | number:'2.2-2' }}</h2>-->
+<!--      <hr>-->
+<!--      <h2>{{ money | currency:'USD':true }}</h2>-->
+      <hr>
+      <h2>{{ date }}</h2>
+      <h2>{{ date | date }}</h2>
+      <h2>{{ date | date:'fullDate' }}</h2>
+      <h2>{{ date | date:'shortDate' }}</h2>
+      <h2>{{ date | date:'shortTime' }}</h2>
+      <hr>
+      <h2>{{ amount }}</h2>
+      <h2>{{ amount | percent }}</h2>
+      <hr>
+      <h2>{{ object }}</h2>
+      <h2><pre>{{ object | json }}</pre></h2>
+    </div>
+  `
 })
 export class AppComponent {
-  headerText = 'Создание новой директивы'
-  items = [1, 2, 3, 4, 5];
-  current = 1;
+  // headerText = 'Создание новой директивы'
+  // items = [1, 2, 3, 4, 5];
+  // current = 1;
+  //
+  // onClick(number: number) {
+  //   this.current = number;
+  // }
 
-  onClick(number: number) {
-    this.current = number;
+  name = 'WebForMyself'
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested :
+      {
+        xyz: 3,
+        numbers: [1, 2, 3]
+      }
   }
 }
