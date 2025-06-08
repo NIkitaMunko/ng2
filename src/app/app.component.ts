300fc6fd67af252996be2f4df6004ff2fa969631
Nikita Munko <nikmoon16@gmail.com>
2025-06-08 10:57:30 +0200

    b4l5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import {Component} from '@angular/core';
 import {BackgroundDirective} from './directives/background.directive';
+import {NgForOf, NgSwitch, NgSwitchCase, NgSwitchDefault} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [
-    BackgroundDirective
+    BackgroundDirective,
+    NgForOf,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   headerText = 'Создание новой директивы'
+  items = [1, 2, 3, 4, 5];
+  current = 1;
+
+  onClick(number: number) {
+    this.current = number;
+  }
 }
