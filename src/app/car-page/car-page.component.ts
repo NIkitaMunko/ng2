e1851edcd321da292343bd903480e345392a042c
Nikita Munko <nikmoon16@gmail.com>
2025-06-24 09:25:23 +0200

    b9l6

--- src/app/car-page/car-page.component.ts
+++ src/app/car-page/car-page.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Params, RouterLink} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-car-page',
@@ -13,18 +13,39 @@ export class CarPageComponent implements OnInit {
 
   id: number = 0;
   name: string = '';
+  color: string = '';
+  year: string = '';
+  hash: string | null = '';
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
   }
 
   ngOnInit(): void {
-        this.id = +this.route.snapshot.params['id'];
-        this.name = this.route.snapshot.params['name'];
+    this.id = +this.route.snapshot.params['id'];
+    this.name = this.route.snapshot.params['name'];
+    this.color = this.route.snapshot.queryParams['color'];
+    this.year = this.route.snapshot.queryParams['year'];
+    this.hash = this.route.snapshot.fragment;
 
-        this.route.params.subscribe((params: Params) => {
-          this.id = +params['id'];
-          this.name = params['name'];
-        })
-    }
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.name = params['name'];
+    })
+
+    this.route.queryParams.subscribe((params: Params) => {
+      this.color = params['color'];
+      this.year = params['year'];
+    })
+  }
+
+  openMazdaPage() {
+    this.router.navigate(['./cars', '8', 'Mazda'], {
+      queryParams: {
+        color: 'pink',
+        year: 1995,
+      },
+      fragment: 'pic'
+    });
+  }
 
 }
