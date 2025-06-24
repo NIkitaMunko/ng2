35857a5e3ade3e9f5f73837d6ec26d20259739e1
Nikita Munko <nikmoon16@gmail.com>
2025-06-24 09:38:21 +0200

    b9l8

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import {CarsPageComponent} from './cars-page/cars-page.component';
 import {HomePageComponent} from './home-page/home-page.component';
 import {NgModule} from '@angular/core';
 import {CarPageComponent} from './car-page/car-page.component';
+import {NotFountComponent} from './not-fount/not-fount.component';
 
 export const routes: Routes = [
   {path: 'cars', component: CarsPageComponent, children: [
       {path: ':id/:name', component: CarPageComponent},
     ]},
   {path: '', component: HomePageComponent},
+  {path: 'not-found', component: NotFountComponent},
+  {path: '**', redirectTo: '/not-found'},
 ];
 
 @NgModule({
