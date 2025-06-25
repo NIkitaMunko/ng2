ea215e9c8c67896d77355559d626ed9d4d5404c0
Nikita Munko <nikmoon16@gmail.com>
2025-06-25 12:01:08 +0200

    b11l3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,12 +24,34 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
       transition('start <=> end', animate('800ms ease-in')),
       transition('start => active', animate(400)),
       transition('active => end', animate(400)),
+    ]),
+    trigger('multi', [
+      state('start', style({
+        width: '150px',
+        height: '150px',
+        border: '5px solid black',
+      })),
+      state('end', style({
+        width: '170px',
+        height: '170px',
+        backgroundColor: 'blue',
+      })),
+      transition('start <=> end', [
+        style({
+          backgroundColor: 'red',
+        }),
+        animate(1500, style({
+          backgroundColor: 'yellow',
+        })),
+        animate(1000)
+      ]),
     ])
   ]
 })
 export class AppComponent {
 
   clickedDivState: string = 'start';
+  multiState: string = 'start';
 
   changeDivState() {
     this.clickedDivState = 'end';
