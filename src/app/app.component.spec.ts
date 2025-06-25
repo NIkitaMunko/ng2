0602e55fe1542f1304029cd06e68dcb78715fee3
Nikita Munko <nikmoon16@gmail.com>
2025-06-25 13:09:48 +0200

    b12l1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,12 +13,12 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-  //
-  // it(`should have the 'ang-vl-m' title`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('ang-vl-m');
-  // });
+
+  it(`should have the 'ang-vl-m' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ang-vl-m');
+  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,12 @@
 import {Component} from '@angular/core';
-import {NgIf} from '@angular/common';
 import {changeWithTrigger, divTrigger} from './app.animations';
-import {AnimationEvent} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [
-    NgIf
-  ],
+  imports: [],
   animations: [divTrigger, changeWithTrigger]
 })
 export class AppComponent {
-  isVisible: boolean = false;
-
-  onAnimationStart(event: AnimationEvent) {
-    console.log('animation start', event);
-  }
-
-  onAnimationDone(event: AnimationEvent) {
-    console.log('animation done', event);
-  }
+  title: string = "ang-vl-m";
 }
